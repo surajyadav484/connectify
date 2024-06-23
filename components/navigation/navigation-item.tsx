a2b7e7939a88468515d053231bca0d451118e770
surajyadav484 <surajryadav20@gmail.com>
2024-06-23 13:37:08 +0530

    Added side navigation bar

---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import ActionTooltip from "../action-tooltip";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+interface NavigationItemProps {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+
+  return (
+    <ActionTooltip align="center" side="right" label={name}>
+      <button onClick={onClick} className="group relative flex items-center">
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params.serverId === id ? "h-[36px]" : "h-[8px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params.serverId === id &&
+              "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image fill src={imageUrl} alt="Channel" />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+};
+
+export default NavigationItem;
